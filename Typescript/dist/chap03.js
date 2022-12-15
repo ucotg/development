e8f97b4687012732395ae3ced0ce93121455aa33
ucchie <dbowserj@gmail.com>
2022-12-16 01:45:40 +0900

    登録

---
+++ Typescript/dist/chap03.js
@@ -0,0 +1,26 @@
+import { createInterface } from "readline";
+const propName = "foo";
+const obj = {
+    [propName]: 123,
+    "foo bar": -500,
+    "↑↓↑↓": "",
+    1: "one",
+    2.05: "two point five"
+};
+console.log(obj["foo"]);
+console.log(obj["foo bar"]);
+console.log(obj["1"]);
+console.log(obj["2.05"]);
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const messages = {
+    good: "0以上の数値が入力されました!",
+    bad: "負の数値を入力しないでください!"
+};
+rl.question("数値を入力してください:", (line) => {
+    const num = Number(line);
+    console.log(messages[num >= 0 ? "good" : "bad"]);
+    rl.close();
+});
---
+++ Typescript/dist/literal.js
@@ -0,0 +1,15 @@
+"use strict";
+const binary = 0b1010; //2進数は先頭に0b、8進数は0o、16進数は0x
+const octal = 0o755;
+const hexadecimal = 0xff;
+console.log(binary, octal, hexadecimal);
+const big = 1e8; //指数標記
+const small = 4e-5;
+console.log(big, small);
+console.log("Hello, \u{796d} World!");
+const no = false;
+const yes = true;
+console.log(no, yes);
+const x = "123,456,789".split(",");
+console.log(x);
+console.log(typeof (x));

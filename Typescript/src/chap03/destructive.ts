00f01b82e76a8e04e8d2b6f371cb1eaf0b37f097
ucchie <dbowserj@gmail.com>
2022-12-18 01:29:32 +0900

    TSブルーベリー本3.6

---
+++ Typescript/src/chap03/destructive.ts
@@ -0,0 +1,21 @@
+const obj = {
+  str: "Hello, world!",
+  num: 1234,
+};
+
+const nested = {
+  num: 123,
+  obj: {
+    foo: "Hello",
+    bar: "world",
+  },
+};
+
+const { str: hello, num } = obj;
+console.log(hello, num);
+const {
+  num: number,
+  obj: { foo: foofoo },
+} = nested;
+
+console.log(number, foofoo);

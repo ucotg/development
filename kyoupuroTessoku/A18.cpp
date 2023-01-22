79ccee4ef19a6bcd051ffe9136ba303a3c09868c
ucchie <dbowserj@gmail.com>
2023-01-22 20:46:16 +0900

    競プロファイル整理

---
+++ kyoupuroTessoku/A18.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    int n, s;
+    cin >> n >> s;
+    int a[n + 1];
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+}

548ac178c080cbd39baa28e9f81cb38a281bc946
ucchie <dbowserj@gmail.com>
2023-01-29 04:06:55 +0900

    競プロ

--- kyoupuroTessoku/A18.cpp
+++ kyoupuroTessoku/A18.cpp
@@ -7,9 +7,54 @@ int main()
     int n, s;
     cin >> n >> s;
     int a[n + 1];
+    bool dp[n + 1][s + 1];
 
     for (int i = 1; i <= n; i++)
     {
         cin >> a[i];
+    }
+
+    dp[0][0] = true;
+    for (int i = 1; i <= s; i++)
+    {
+        dp[0][i] = false;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= s; j++)
+        {
+            if (j < a[i])
+            {
+                if (dp[i - 1][j])
+                {
+                    dp[i][j] = true;
+                }
+                else
+                {
+                    dp[i][j] = false;
+                }
+            }
+            if (j >= a[i])
+            {
+                if (dp[i - 1][j] || dp[i - 1][j - a[i]])
+                {
+                    dp[i][j] = true;
+                }
+                else
+                {
+                    dp[i][j] = false;
+                }
+            }
+        }
+    }
+
+    if (dp[n][s])
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
     }
 }
